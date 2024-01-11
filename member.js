dbd7d54da48bda6a587b1a25fe3e7609d25a5a19
Maria Olivia Lihn <Maria_olivia_lihn@mckinsey.com>
2024-01-11 15:42:43 -0300

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember(){
+    var skills = document.getElementById('skills');
+    var skillsMember = document.createElement('div');
+    skillsMember.setAttribute('class', 'member');
+    skillsMember.setAttribute('id', 'skillsMember');
+    skills.appendChild(skillsMember);
+  }
